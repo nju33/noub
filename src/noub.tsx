4c8c31825b15aa33249b425f4f0900cc7085ac62
nju33 <nju33.ki@gmail.com>
2019-01-21 19:34:16 +0900

    feat: wip

--- src/noub.tsx
+++ src/noub.tsx
@@ -1,5 +1,6 @@
 /* @jsx el */
 import {el, mount, RedomComponent} from 'redom';
+import {matchesUA, resolveUserAgent} from 'browserslist-useragent';
 
 declare namespace JSX {
   interface IntrinsicElements {
@@ -11,8 +12,13 @@ export interface NoubOptions {
   browsers: string[];
 }
 
+enum Color {
+  Usubeni = '#e87a90',
+  Kurenai = '#cb1b45'
+}
+
 export class Noub implements RedomComponent {
-  static create(elementId: string, opts: NoubOptions) {
+  static init(elementId: string, opts: NoubOptions) {
     const targetElement = document.getElementById(elementId);
     if (targetElement === null) {
       throw TypeError('対象の要素が存在しません');
@@ -20,16 +26,59 @@ export class Noub implements RedomComponent {
 
     const noub = new Noub();
 
-    mount(targetElement, noub);
+    const browserslistOpts: any = {};
+    if (Array.isArray(opts.browsers)) {
+      browserslistOpts.browsers = opts.browsers;
+    }
+
+    const passed = matchesUA(navigator.userAgent, opts);
+
+    if (!passed) {
+      mount(targetElement, noub);
+    }
 
     return noub;
   }
 
   el: HTMLElement;
 
+  getBrowserName() {
+    const browserVersion = resolveUserAgent(navigator.userAgent);
+    return `${browserVersion.family}(${browserVersion.version})`;
+  }
+
   constructor() {
     // el is not defined
     el;
-    this.el = <div>hoge</div>;
+    this.el = (
+      <div
+        role="alert"
+        style={{
+          background: Color.Usubeni,
+          border: `1px solid ${Color.Kurenai}`,
+          borderRadius: '3px',
+          boxSizing: 'border-box',
+          padding: '0 1em',
+          color: '#292929'
+        }}
+      >
+        <p>
+          お使いのブラウザ
+          <strong style={{color: '#000'}}>「{this.getBrowserName()}」</strong>
+          はサポート対象外です。
+          <br />
+          <a
+            href="https://www.google.com/intl/ja_ALL/chrome/"
+            style={{
+              color: '#000',
+              fontWeight: 'bold'
+            }}
+          >
+            Google Chrome
+          </a>
+          などのモダンブラウザを利用してください。
+        </p>
+      </div>
+    );
   }
 }
--- src/types/jsx/index.d.ts
+++ src/types/jsx/index.d.ts
@@ -1,5 +1,5 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    [index: string]: Partial<HTMLElement>;
+    [index: string]: any;
   }
 }
